73384ade2cbdad7d40eed82b4c59ddc8c027ab57
Marko Mäkelä <marko.makela@mariadb.com>
2017-06-05 11:32:43 +0300

    Mariabackup: Clean up startup and shutdown
    xtrabackup --prepare still fails due to the log file size not being
    a multiple of innodb_page_size, but there will be no memory leaks
    reported.
    
    Tablespace::shutdown(): Clear m_path. This was moved from
    Tablespace::~Tablespace().
    
    LatchDebug::shutdown(): Remove a redundant condition.
    
    innodb_shutdown(): Skip the redo log shutdown in SRV_OPERATION_BACKUP
    mode.
    
    innodb_inited: Remove.
    
    innodb_init_param(): Set srv_buf_pool_instances.
    
    innodb_init(): If innobase_start_or_create_for_mysql() fails,
    call innodb_shutdown().
    
    xtrabackup_backup_func(): Invoke innodb_shutdown() instead of
    shutting down the individual subsystems.
    
    innodb_free_param(): Call srv_sys_space.shutdown().

--- storage/innobase/srv/srv0start.cc
+++ storage/innobase/srv/srv0start.cc
@@ -1468,10 +1468,6 @@ innobase_start_or_create_for_mysql(void)
 		srv_read_only_mode = true;
 	}
 
-	if (srv_force_recovery == SRV_FORCE_NO_LOG_REDO) {
-		srv_read_only_mode = 1;
-	}
-
 	high_level_read_only = srv_read_only_mode
 		|| srv_force_recovery > SRV_FORCE_NO_TRX_UNDO;
 
@@ -2794,25 +2790,27 @@ innodb_shutdown()
 {
 	ut_ad(!srv_running);
 
-	if (srv_fast_shutdown) {
-		srv_shutdown_bg_undo_sources();
+	switch (srv_operation) {
+	case SRV_OPERATION_BACKUP:
+		break;
+	case SRV_OPERATION_NORMAL:
+	case SRV_OPERATION_RESTORE:
+		if (srv_fast_shutdown) {
+			srv_shutdown_bg_undo_sources();
+		}
+
+		/* Shut down the persistent files. */
+		logs_empty_and_mark_files_at_shutdown();
+
+		if (ulint n_threads = srv_conc_get_active_threads()) {
+			ib::warn() << "Query counter shows "
+				   << n_threads << " queries still"
+				" inside InnoDB at shutdown";
+		}
+
+		/* Exit any remaining threads. */
+		srv_shutdown_all_bg_threads();
 	}
-
-	/* 1. Flush the buffer pool to disk, write the current lsn to
-	the tablespace header(s), and copy all log data to archive.
-	The step 1 is the real InnoDB shutdown. The remaining steps 2 - ...
-	just free data structures after the shutdown. */
-
-	logs_empty_and_mark_files_at_shutdown();
-
-	if (ulint n_threads = srv_conc_get_active_threads()) {
-		ib::warn() << "Query counter shows "
-			<< n_threads << " queries still"
-			" inside InnoDB at shutdown";
-	}
-
-	/* 2. Make all threads created by InnoDB to exit */
-	srv_shutdown_all_bg_threads();
 
 	if (srv_monitor_file) {
 		fclose(srv_monitor_file);
